552a46c79a9480b1544f1871694a030f58c8c26d
Noe <noe.duruz@gmail.com>
2024-10-06 20:03:35 +0200

    added fermat factorization

--- largest-prime-factor/src/main.rs
+++ largest-prime-factor/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    let number: u64 = 600851475143;
-    // let number = 13195;
-    let factors = prime_factors(number);
+    // let number: u64 = 600851475143;  // [71, 839, 1471, 6857];
+    let number = 13195;  // [5, 7, 13, 29];
+    // let factors = trial_division(number);
+    let factors = fermat_factorization(number);
     println!("{factors:#?}");
 }
 
@@ -15,14 +16,20 @@ mod tests {
     const FACTORS2: [u64; 4] = [71, 839, 1471, 6857];
 
     #[test]
-    fn test_prime_factors() {
-        assert_eq!(prime_factors(NUM1), FACTORS1.to_vec());
-        assert_eq!(prime_factors(NUM2), FACTORS2.to_vec());
+    fn test_trial_division() {
+        assert_eq!(trial_division(NUM1), FACTORS1.to_vec());
+        assert_eq!(trial_division(NUM2), FACTORS2.to_vec());
+    }
+
+    #[test]
+    fn test_fermat_factorization() {
+        assert_eq!(fermat_factorization(NUM1), FACTORS1.to_vec());
+        assert_eq!(fermat_factorization(NUM2), FACTORS2.to_vec());
     }
 }
 
 // https://stackoverflow.com/a/412942
-pub fn prime_factors(x: u64) -> Vec<u64> {
+pub fn trial_division(x: u64) -> Vec<u64> {
     let mut factors = vec![];
     let mut d = 2;
     let mut n = x;
@@ -43,4 +50,45 @@ pub fn prime_factors(x: u64) -> Vec<u64> {
         }
     }
     return factors;
+}
+
+pub fn fermat_factorization(x: u64) -> Vec<u64> {
+    // Divide by 2 until x is odd
+    let mut partial_factors = vec![];
+    let mut factors = vec![];
+    let mut x = x;
+    while x % 2 == 0 {
+        factors.push(2);
+        x /= 2;
+    }
+    let x = x; // x is odd and immutable
+    partial_factors.push(x);
+    while partial_factors.len() > 0 {
+        let x = partial_factors.pop().unwrap();
+        let (a, b) = fermat_factor(x);
+        match a {
+            1 => {
+                factors.push(b);
+            },
+            _ => {
+                partial_factors.push(a);
+                partial_factors.push(b);
+            }
+        }
+    }
+    factors.sort();
+    factors
+}
+
+pub fn fermat_factor(x: u64) -> (u64, u64) {
+    assert_eq!(x % 2, 1);
+    let mut a = (x as f64).sqrt().ceil();
+    let mut b2 = a * a - x as f64;
+    while b2.sqrt() % 1.0 != 0.0 {
+        a += 1.0;
+        b2 = a * a - x as f64;
+    }
+    let a = a as u64;
+    let b = b2.sqrt() as u64;
+    (a - b, a + b)
 }
